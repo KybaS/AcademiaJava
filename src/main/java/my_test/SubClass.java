8b28513fef3bfed18a70b8f2ca6b9c7eb25468df
KybaS <sergeykiba2012@gmail.com>
2018-07-02 13:16:09 +0300

    return random result from method

---
+++ src/main/java/my_test/SubClass.java
@@ -0,0 +1,28 @@
+package my_test;
+
+public class SubClass {
+
+    private int a = 0, b = 11;
+    int random_number = a + (int) (Math.random() * b);
+
+    private double plus(double z, double... x){
+        return z + x[random_number];
+    }
+
+    protected void numbersRand(double z, double... x){
+        double res = plus(z, x);
+        System.out.println(res);
+    }
+
+     String[] generatorStrings(String... strings){
+        String[] sentence = strings;
+        return sentence;
+    }
+
+    protected void stringsRand(String... strings){
+        String[] result = generatorStrings(strings);
+        System.out.println(result[random_number]);
+    }
+
+
+}
---
+++ src/main/java/my_test/SuperClass.java
@@ -0,0 +1,41 @@
+package my_test;
+
+import java.util.ArrayList;
+
+public class SuperClass extends SubClass{
+    public static void main(String[] args) {
+
+        SubClass subClass = new SubClass();
+
+        subClass.numbersRand(1000, 0, 100, 200.78, 300, 400, 500, 600, 700, 800, 900, 1000, 1100);
+        subClass.stringsRand("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
+
+        ArrayList myStrings = new ArrayList(5);
+        myStrings.add(0, "arrayZero");
+        myStrings.add(1, "arrayOne");
+        myStrings.add(2, "arrayTwo");
+        myStrings.add(3, "array3");
+        myStrings.add(4, "array4");
+        myStrings.add(5, "array5");
+        myStrings.add(6, "array6");
+        myStrings.add(7, "array7");
+        myStrings.add(8, "array8");
+        myStrings.add(9, "array9");
+        myStrings.add(10, "array10");
+        myStrings.add(11, "array11");
+
+        int size = myStrings.size();
+        String res = myStrings.get(2).toString();
+        System.out.println(res + " " + size);
+
+
+        int count = 0;
+        for (int i = 0; i < 5; i++){
+           String result = myStrings.get(subClass.random_number).toString();
+           subClass.random_number = subClass.random_number + count;
+           count++;
+            System.out.println(result + " " + count);
+
+        }
+    }
+}
