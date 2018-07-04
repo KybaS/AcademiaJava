7be02a225e34498d3b031758c66f44bcc0e6d924
skyba <ksv@evogence.com>
2018-07-04 23:42:02 +0300

    while loop

---
+++ src/main/java/while_test/WhileClass.java
@@ -0,0 +1,27 @@
+package while_test;
+
+import java.util.Scanner;
+
+public class WhileClass {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int number = 0;
+
+        while (number != 7) {
+            System.out.println("Enter number between 0 - 10");
+            number = scan.nextInt();
+        }
+        System.out.println("Congratulations on your guessed number!");
+
+
+        Scanner nextScan = new Scanner(System.in);
+        String word = " ";
+
+        while (!word.equals("tea")){
+            System.out.println("I want some tea!!! give me tea!!!");
+            word = nextScan.nextLine();
+        }
+        System.out.println("Thank you for your attention and tea)");
+    }
+}
