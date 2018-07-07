9e9e679288ffcb342249375612d86068941cd10e
skyba <ksv@evogence.com>
2018-07-08 00:31:54 +0300

    do while

---
+++ src/main/java/while_test/DoWhile.java
@@ -0,0 +1,13 @@
+package while_test;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        int number = 0;
+
+        do {
+            System.out.println(number);
+            number++;
+        }while (number <= 6);
+    }
+}
