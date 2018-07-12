9b92040206fcb55ac789ada246a719ecc27874de
skyba <ksv@evogence.com>
2018-07-12 22:55:18 +0300

    arrays with methods

--- src/main/java/arrays/Arrays.java
+++ src/main/java/arrays/Arrays.java
@@ -3,16 +3,38 @@ package arrays;
 public class Arrays {
     public static void main(String[] args) {
 
-        int[] numbers = {5, 7, 100, 505};
+        int[] numbers = {5, 7, 100, -505, 0, -9};
         numbers[1] = 777;
-
-        System.out.println(numbers[0] + " " + numbers[1] + " " + numbers[2]);
-
 
         String[] summerMonths = {"June", "July", "August"};
 
+        System.out.println(numbers[0] + " " + numbers[1] + " " + numbers[2]);
+
         for (String summerMonth : summerMonths) {
-            System.out.print(summerMonth + " / ");
+            System.out.print(summerMonth + "\t" + "\n");
+        }
+
+        for (int number : numbers) {
+            sumOfArray(number);
+            recheckNumValue(number);
+        }
+
+    }
+
+    public static void sumOfArray(int num) {
+        int result = num * 2;
+        System.out.print("x2 = " + result + "\t");
+    }
+
+    public static void recheckNumValue(int a) {
+        if (a < 0) {
+            System.out.println("x1 = " + a + " number has a negative value");
+        } else if (a > 0){
+            System.out.println("x1 =  " + a + " number has a positive value");
+        } else if (a == 0){
+            System.out.println("This is zero");
+        } else {
+            System.out.println("ERROR which will never to being");
         }
     }
 }
