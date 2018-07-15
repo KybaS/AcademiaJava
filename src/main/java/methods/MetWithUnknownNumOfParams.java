d7360d079fcbf3996d6d69ebe28e5b27f6e5883a
skyba <ksv@evogence.com>
2018-07-15 11:01:13 +0300

    methods with unknown (a lot of) number of parameters

--- src/main/java/methods/MetWithUnknownNumOfParams.java
+++ src/main/java/methods/MetWithUnknownNumOfParams.java
@@ -5,6 +5,7 @@ public class MetWithUnknownNumOfParams {
 
         System.out.println(calculatingAverageOfNum(23,34,54,344,43,3456,34534,34534,23,45,345,435.45,345.43));
         System.out.println(numOfPassedParams("test", "words", "what are you talking about?"));
+        printListOfNumbers(45,23,234,34,4554,34,32,2,21,103,545);
     }
 
     public static double calculatingAverageOfNum(double...numbers){
@@ -17,5 +18,10 @@ public class MetWithUnknownNumOfParams {
     public static String numOfPassedParams(String...words){
            int num = words.length;
         return String.valueOf(num);
+    }
+
+    public static void printListOfNumbers(int...ints){
+        for (int numbers:ints)
+        System.out.print(numbers + "\t");
     }
 }
