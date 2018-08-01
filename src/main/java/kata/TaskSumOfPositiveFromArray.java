303fbe9505e969995f673bc91e66666f06e41696
KybaS <sergeykiba2012@gmail.com>
2018-08-02 00:20:21 +0300

    unit tests for sum method (SolutionTest)

--- src/main/java/kata/TaskSumOfPositiveFromArray.java
+++ src/main/java/kata/TaskSumOfPositiveFromArray.java
@@ -1,11 +1,18 @@
 package kata;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TaskSumOfPositiveFromArray {
     public static void main(String[] args) {
 
         int[] numbers = {5, -505, 0, -9, 1};
 
         System.out.println(sum(numbers));
+
+        SolutionTest test = new SolutionTest();
+        test.testSumOfPositiveNumbersFromArray();
     }
 
     public static int sum(int[] numbers) {
@@ -14,5 +21,22 @@ public class TaskSumOfPositiveFromArray {
             if (i >= 1) sum += i;
         }
         return sum;
+    }
+
+    public static class SolutionTest {
+        @Test
+        public void testSumOfPositiveNumbersFromArray() {
+            assertEquals(25, TaskSumOfPositiveFromArray.sum(new int[]{5, 10, 10}));
+            System.out.println("test with all positive numbers is passed");
+
+            assertEquals(30, TaskSumOfPositiveFromArray.sum(new int[]{10, -15, 5, 2, 3, 10}));
+            System.out.println("test with one negative and other positive numbers is passed");
+
+            assertEquals(0, TaskSumOfPositiveFromArray.sum(new int[]{}));
+            System.out.println("test without numbers is passed");
+
+            assertEquals(0, TaskSumOfPositiveFromArray.sum(new int[]{-1, -100, -34}));
+            System.out.println("test with all negative numbers is passed");
+        }
     }
 }
