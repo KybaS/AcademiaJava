77c3b4f1a9e4e50703a84a2185aeca10c1f89c7a
KybaS <sergeykiba2012@gmail.com>
2018-08-02 22:39:15 +0300

    new method isThisTriangle unit tests for isThisTriangle

--- src/main/java/kata/BooleanToString.java
+++ src/main/java/kata/BooleanToString.java
@@ -2,7 +2,7 @@ package kata;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class BooleanToString {
     public static void main(String[] args) {
@@ -10,31 +10,50 @@ public class BooleanToString {
         System.out.println(booleanToString(false));
         System.out.println(booleanToString(true));
 
-        BoolTest boolTest = new BoolTest();
-        boolTest.testBoolToString();
+        System.out.println(isThisTriangle(10,0,30));
+
+        UnitTestMyMethods unitTestMyMethods = new UnitTestMyMethods();
+        unitTestMyMethods.testBoolToString();
+        unitTestMyMethods.testIsThisTriangle();
     }
 
-     private static String booleanToString(boolean YesNo){
+    private static String booleanToString(boolean YesNo) {
 
         // return YesNo ? "Yes" : "No";
 
         String yes = "Yes";
         String no = "No";
-        if (YesNo){
+        if (YesNo) {
             return yes;
-        }else {
+        } else {
             return no;
         }
     }
 
-    public static class BoolTest{
+    private static boolean isThisTriangle(int a, int b, int c) {
+        return a + b > c && a + c > b && b + c > a;
+    }
+
+    public static class UnitTestMyMethods {
         @Test
-        public void testBoolToString(){
+        public void testBoolToString() {
             assertEquals(BooleanToString.booleanToString(true), "Yes");
             System.out.println("boolean true == String yes");
 
             assertEquals(BooleanToString.booleanToString(false), "No");
             System.out.println("boolean false == String no");
+        }
+
+        @Test
+        public void testIsThisTriangle(){
+            assertTrue(isThisTriangle(3, 4, 6));
+            System.out.println("Positive test is passed");
+
+            assertFalse(isThisTriangle(9, -5, 4));
+            System.out.println("negative test with [-] is passed");
+
+            assertFalse(isThisTriangle(0, 5, 100));
+            System.out.println("negative test with zero is passed");
         }
     }
 }
