0ca90c242eba5c97e42b82f79f6afd78d36e3717
KybaS <sergeykiba2012@gmail.com>
2018-08-05 22:57:27 +0300

    leet speak

---
+++ src/main/java/kata/MainLeetSpeak.java
@@ -0,0 +1,9 @@
+package kata;
+
+public class MainLeetSpeak {
+    private static final String QA = "QUALITY ASSURANCE";
+
+    public static void main(String[] args) {
+        System.out.println("Translate to leet: " + LeetSpeak.toLeetSpeak(QA));
+    }
+}
