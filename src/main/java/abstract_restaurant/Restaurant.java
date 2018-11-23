b282b20a4d8598556275919c36b6a338a162e90f
skyba <ksv@evogence.com>
2018-11-23 15:14:37 +0200

    refactoring

--- src/main/java/abstract_restaurant/Restaurant.java
+++ src/main/java/abstract_restaurant/Restaurant.java
@@ -4,7 +4,7 @@ public class Restaurant {
     public static void main(String[] args) {
 
         Client client = new Client("John");
-        Dish chicken = new Chicken(MethodOfCooking.FRIED);
+        Dish chicken = new Chicken(MethodOfCooking.BOILED);
         Dish grass = new Salad(Grass.PARSLEY);
         client.eat(chicken, grass);
     }
--- src/main/java/deposit_counter/PercentOnMyDepo.java
+++ src/main/java/deposit_counter/PercentOnMyDepo.java
@@ -2,7 +2,7 @@ package deposit_counter;
 
 public class PercentOnMyDepo {
     public static void main(String[] args) {
-        double deposit = 10000, percent = 0.11, balance = 0;
+        double deposit = 10000, percent = 0.14, balance = 3000;
         int yearOfDepo = 2018;
         for (int year = yearOfDepo; year <= (yearOfDepo + 1); year++) {
             balance = deposit * Math.pow(1 + percent, (year - yearOfDepo + 1));
